99bafcecaf3760315b62dd00a23132398129c0b3
Fernanda <fernandasoares.reis1@gmail.com>
2022-01-18 14:44:44 -0300

    minha primeira aplicação spring - 17.01

---
+++ atividadeSpringBoot1/src/main/java/com/helloworld/atividade/controller/AtividadeController.java
@@ -0,0 +1,21 @@
+package com.helloworld.atividade.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AtividadeController {
+
+	@RequestMapping("/atividade1")
+	@GetMapping
+	public String Atividade1() {
+		return "Habilidades e mentalidades utilizadas: Atenção aos detalhes, Persistência e Mentalidade de Crescimento." ;
+	}
+	
+	@RequestMapping("/atividade2")
+	@GetMapping
+	public String Atividade2() {
+		return "Objetivos de aprendizagem: Conhecimentos básicos em MySQL, SpringBoot.";
+	}
+}
---
+++ atividadeSpringBoot1/src/test/java/com/helloworld/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
